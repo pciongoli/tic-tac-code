b4e2aa3c8fe48afb4b7c17a225f89966157ade5f
pcion <patrick.ciongoli@gmail.com>
2023-02-21 13:57:49 -0500

    updated client

--- public/client.js
+++ public/client.js
@@ -1,73 +1,41 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const path = require("path");
+const board = document.querySelector(".board");
+const message = document.querySelector(".message");
+const cells = document.querySelectorAll(".cell");
+let gameState;
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const socket = io("http://localhost:3000");
 
-app.use(express.static(path.join(__dirname, "public")));
+socket.on("connect", () => {
+   console.log("Connected to server");
+});
 
-let gameState = {
-   board: ["", "", "", "", "", "", "", "", ""],
-   player: "X",
-   winner: null,
-};
+socket.on("gameState", (state) => {
+   gameState = state;
+   render();
+});
 
-io.on("connection", (socket) => {
-   console.log("A user has connected");
-
-   // Send the initial game state to the new player
-   socket.emit("gameState", gameState);
-
-   // Handle incoming moves
-   socket.on("move", (index) => {
-      // Update the game state with the new move
-      gameState.board[index] = gameState.player;
-
-      // Check for a winner
-      if (checkForWinner(gameState.board)) {
-         gameState.winner = gameState.player;
-      } else if (checkForTie(gameState.board)) {
-         gameState.winner = "tie";
+board.addEventListener("click", (event) => {
+   if (event.target.classList.contains("cell")) {
+      const index = event.target.dataset.index;
+      if (gameState.board[index] === "" && !gameState.winner) {
+         socket.emit("move", index);
       }
-
-      // Switch to the other player's turn
-      gameState.player = gameState.player === "X" ? "O" : "X";
-
-      // Broadcast the new game state to all players
-      io.emit("gameState", gameState);
-   });
-
-   // Handle disconnections
-   socket.on("disconnect", () => {
-      console.log("A user has disconnected");
-   });
+   }
 });
 
-function checkForWinner(board) {
-   const winningCombos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-   ];
+function render() {
+   for (let i = 0; i < gameState.board.length; i++) {
+      cells[i].textContent = gameState.board[i];
+   }
 
-   return winningCombos.some((combo) => {
-      const [a, b, c] = combo;
-      return board[a] && board[a] === board[b] && board[b] === board[c];
-   });
+   if (gameState.winner) {
+      if (gameState.winner === "tie") {
+         message.textContent = "It's a tie!";
+      } else {
+         message.textContent = `Player ${gameState.winner} wins!`;
+      }
+      message.classList.remove("hidden");
+   } else {
+      message.classList.add("hidden");
+   }
 }
-
-function checkForTie(board) {
-   return board.every((cell) => cell !== "");
-}
-
-server.listen(3000, () => {
-   console.log("Server started on port 3000");
-});
