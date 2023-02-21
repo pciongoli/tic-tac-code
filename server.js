7eb36ae554e4eb5a987586b755d57178d30d3e9c
pcion <patrick.ciongoli@gmail.com>
2023-02-21 14:41:49 -0500

    issues

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server, {
-   transports: ["websocket"],
+const io = socketIo(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+   console.log(`Server started on port ${port}`);
 });
 
 app.use(express.static(path.join(__dirname, "public")));
