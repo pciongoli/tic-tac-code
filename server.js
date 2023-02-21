7e56981578ddeca08c3c6b6972a0adce0af4022d
pcion <patrick.ciongoli@gmail.com>
2023-02-21 15:23:55 -0500

    tests

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ server.listen(port, () => {
    console.log(`Server started on port ${port}`);
 });
 
+app.get("/", (req, res) => {
+   res.sendFile(__dirname + "/public/index.html");
+});
+
+server.listen(3000, () => {
+   console.log("listening on *:3000");
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 let gameState = {
